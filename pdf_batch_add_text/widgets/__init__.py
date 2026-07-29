d370a9a938813c1d03f3e3d868a4b3f9350a1344
Ethan-19980224 <1316404432@qq.com>
2026-07-29 22:05:29 +0800

    v4.1.0 - 首次完整提交

---
+++ pdf_batch_add_text/widgets/__init__.py
@@ -0,0 +1,5 @@
+"""自定义 Widget 和 Delegate 模块"""
+from .text_column_delegate import TextColumnDelegate
+from .smart_suggest_widget import SmartSuggestWidget
+
+__all__ = ['TextColumnDelegate', 'SmartSuggestWidget']
